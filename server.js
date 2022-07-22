41fcdc3ccb2fd10a780b285575e21ae5c623d7cc
Austin Limanek <austin.limanek@gmail.com>
2022-07-21 22:18:42 -0700

    corrected the path property on the movie data

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/movies', (request, response) => {
           "overview": element.overview,
           "average_votes": element.vote_average,
           "total_votes": element.vote_count,
-          "image_url": "https://image.tmdb.org/t/p/w500" + element.poster_pat,
+          "image_url": "https://image.tmdb.org/t/p/w500" + element.poster_path,
           "popularity": element.popularity,
           "released_on": element.release_date
         });
